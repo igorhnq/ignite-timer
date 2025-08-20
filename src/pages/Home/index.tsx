68b33a7771840a8f0fef82fd6cce26599c115e0a
igorhnq <igorhenrique102@gmail.com>
2025-08-19 21:59:57 -0300

    feat: implement timer countdown funcionality

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useState } from "react";
 import { PlayIcon } from "@phosphor-icons/react";
-import { useState } from "react";
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
+
+import { differenceInSeconds } from "date-fns";
 
 import { 
     CountdownContainer, 
@@ -44,12 +46,25 @@ export function Home() {
             minutesAmount: 0,
         },
     });
+    
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+    useEffect(() => {
+        if (activeCycleId) {
+            setInterval(() => {
+                setAmountSecondsPassed(
+                    differenceInSeconds(new Date(), new Date(activeCycle.startDate)),
+                );
+            }, 1000);
+        }
+    }, [activeCycle]);
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         const newCycle: Cycle = {
             id: String(new Date().getTime()),
             task: data.task,
             minutesAmount: data.minutesAmount,
+            startDate: new Date(),
         };
 
         setCycles((state) => [...state, newCycle]);
@@ -58,7 +73,6 @@ export function Home() {
         reset();
     }
 
-    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
     const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;

53ff862ec5a18d6745a1aacd4f0c150f27f8f4dd
igorhnq <igorhenrique102@gmail.com>
2025-08-26 21:46:45 -0300

    fix: implement reset form from react-hook-form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -40,19 +40,22 @@ export function Home() {
     });
 
     const { handleSubmit, watch, reset } = newCycleForm;
+
+    function handleCreateNewCycle(data: NewCycleFormData) {
+        createNewCycle(data);
+        reset();
+    }
     
     const task = watch("task");
     const isSubmitDisabled = !task;
 
     return (
         <HomeContainer>
-            <form onSubmit={handleSubmit(createNewCycle)}>
-                <CyclesContext.Provider value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished, amountSecondsPassed, setSecondsPassed }}>
-                    <FormProvider {...newCycleForm}>
-                        <NewCycleForm />
-                    </FormProvider>
-                    <Countdown />
-                </CyclesContext.Provider>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+                <FormProvider {...newCycleForm}>
+                    <NewCycleForm />
+                </FormProvider>
+                <Countdown />
                 {activeCycle ? (
                     <StopCountdownButton type="button" onClick={interruptCurrentCycle}>
                         <HandPalmIcon size={24} />
